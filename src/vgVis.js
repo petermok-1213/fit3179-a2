15fa987860f89b051184d9fabf3f4f2b86b413c8
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-10-15 23:34:17 +1100

    FIXED path to vg.json

--- src/vgVis.js
+++ src/vgVis.js
@@ -3,15 +3,7 @@ window.onload = (event) => {
     let mapVis = "./vg/mean_salary.vg.json"
     let yearMeanLineChart = "./vg/year_mean_salary.vg.json"
 
-    vegaEmbed("#vg1", vis1).then((result) => {
-
-    }).catch(console.error)
-
-    vegaEmbed("#vg-map", mapVis).then((result) => {
-
-    }).catch(console.error)
-
-    vegaEmbed("#salary-year-country", yearMeanLineChart).then((result) => {
-
-    }).catch(console.error)
+    vegaEmbed("#vg1", vis1).then((result) => {}).catch(console.error)
+    vegaEmbed("#vg-map", mapVis).then((result) => {}).catch(console.error)
+    vegaEmbed("#salary-year-country", yearMeanLineChart).then((result) => {}).catch(console.error)
 }
