3a4d7ba3360172aeda6bc7a4e386378e2fa64cbf
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-10-16 02:04:20 +1100

    ADDED top/bot5 vis to index

--- src/vgVis.js
+++ src/vgVis.js
@@ -1,9 +1,9 @@
 window.onload = (event) => {
-    let vis1 = "./vg/avgsal_workyear.vg.json"
     let mapVis = "./vg/mean_salary.vg.json"
-    let yearMeanLineChart = "./vg/year_mean_salary.vg.json"
+    let top5 = "./vg/top5_country_year.vg.json"
+    let bot5 = "./vg/bot5_country_year.vg.json"
 
-    vegaEmbed("#vg1", vis1).then((result) => {}).catch(console.error)
     vegaEmbed("#vg-map", mapVis).then((result) => {}).catch(console.error)
-    vegaEmbed("#salary-year-country", yearMeanLineChart).then((result) => {}).catch(console.error)
+    vegaEmbed("#top-5", top5).then((result) => {}).catch(console.error)
+    vegaEmbed("#bot-5", bot5).then((result) => {}).catch(console.error)
 }
