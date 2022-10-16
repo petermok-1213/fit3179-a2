914415f3a8151803e0b68fbed59590f0cb4e73e4
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-10-17 01:43:02 +1100

    ADDED barchart to index

--- src/vgVis.js
+++ src/vgVis.js
@@ -2,8 +2,11 @@ window.onload = (event) => {
     let mapVis = "./vg/mean_salary.vg.json"
     let top5 = "./vg/top5_country_year.vg.json"
     let bot5 = "./vg/bot5_country_year.vg.json"
+    let barChart = "./vg/remote_size_salary_bar.vg.json"
 
     vegaEmbed("#vg-map", mapVis).then((result) => {}).catch(console.error)
     vegaEmbed("#top-5", top5).then((result) => {}).catch(console.error)
     vegaEmbed("#bot-5", bot5).then((result) => {}).catch(console.error)
+    vegaEmbed("#size-remote-bar", barChart).then((result) => {}).catch(console.error)
+
 }
