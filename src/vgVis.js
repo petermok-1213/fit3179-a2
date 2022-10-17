790a1606c211823d1c0c06e42e0009afa0955b63
Peter.M <43172966+Peter-theMok@users.noreply.github.com>
2022-10-17 18:39:34 +1100

    ADDED campany size chart

--- src/vgVis.js
+++ src/vgVis.js
@@ -2,11 +2,13 @@ window.onload = (event) => {
     let mapVis = "./vg/mean_salary.vg.json"
     let top5 = "./vg/top5_country_year.vg.json"
     let bot5 = "./vg/bot5_country_year.vg.json"
-    let barChart = "./vg/remote_size_salary_bar.vg.json"
+    let remoteBar = "./vg/remote_bar.vg.json"
+    let companySizeBar = "./vg/company_size_bar.vg.json"
 
     vegaEmbed("#vg-map", mapVis, {"actions": false}).then((result) => {}).catch(console.error)
     vegaEmbed("#top-5", top5, {"actions": false}).then((result) => {}).catch(console.error)
     vegaEmbed("#bot-5", bot5, {"actions": false}).then((result) => {}).catch(console.error)
-    vegaEmbed("#size-remote-bar", barChart, {"actions": false}).then((result) => {}).catch(console.error)
+    vegaEmbed("#remote-bar", remoteBar, {"actions": false}).then((result) => {}).catch(console.error)
+    vegaEmbed("#size-bar", companySizeBar, {"actions": false}).then((result) => {}).catch(console.error)
 
 }
