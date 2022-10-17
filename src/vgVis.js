071424094da0d2ca653aceacb4dd48eb713dad95
Peter.M <43172966+Peter-theMok@users.noreply.github.com>
2022-10-17 16:18:59 +1100

    UDPATED to hide action buttons for all vis

--- src/vgVis.js
+++ src/vgVis.js
@@ -4,9 +4,9 @@ window.onload = (event) => {
     let bot5 = "./vg/bot5_country_year.vg.json"
     let barChart = "./vg/remote_size_salary_bar.vg.json"
 
-    vegaEmbed("#vg-map", mapVis).then((result) => {}).catch(console.error)
-    vegaEmbed("#top-5", top5).then((result) => {}).catch(console.error)
-    vegaEmbed("#bot-5", bot5).then((result) => {}).catch(console.error)
-    vegaEmbed("#size-remote-bar", barChart).then((result) => {}).catch(console.error)
+    vegaEmbed("#vg-map", mapVis, {"actions": false}).then((result) => {}).catch(console.error)
+    vegaEmbed("#top-5", top5, {"actions": false}).then((result) => {}).catch(console.error)
+    vegaEmbed("#bot-5", bot5, {"actions": false}).then((result) => {}).catch(console.error)
+    vegaEmbed("#size-remote-bar", barChart, {"actions": false}).then((result) => {}).catch(console.error)
 
 }
